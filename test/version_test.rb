ea6abaae438b0f95b9b1b2f2718eb12c497496e6
Petar Ćurković <petar.curkovic@infinum.hr>
2016-09-15 22:03:34 +0200

    Updated version and authors

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require_relative 'helpers/test_helper'
 
 class VersionTest < Minitest::Test
   def test_version
-    assert_equal '2.1.0', Enumerations::VERSION
+    assert_equal '2.2.0', Enumerations::VERSION
   end
 end
